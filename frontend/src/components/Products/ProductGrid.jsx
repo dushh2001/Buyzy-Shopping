9015fb3a00bae7b273e20b0093e397687b6d6b0e
dushan <kddalwis@gmail.com>
2025-03-13 19:42:57 +0530

    create the top wears for women section

--- frontend/src/components/Products/ProductGrid.jsx
+++ frontend/src/components/Products/ProductGrid.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const ProductGrid = ({ products }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
       {products.map((product, index) => (
         <Link key={index} to={`/product/${product._id}`} className="block">
           <div className="p-4 bg-white rounded-lg">
