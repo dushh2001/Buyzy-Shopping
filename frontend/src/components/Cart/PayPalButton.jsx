db6b9bac1d93f715d7d102d900f5ddad0a990568
dushan <kddalwis@gmail.com>
2025-04-01 18:03:37 +0530

    create the .env file and complete the frontend part of user

--- frontend/src/components/Cart/PayPalButton.jsx
+++ frontend/src/components/Cart/PayPalButton.jsx
@@ -4,8 +4,7 @@ const PayPalButton = ({ amount, onSuccess, onError }) => {
   return (
     <PayPalScriptProvider
       options={{
-        clientId:
-          "Aal2w2y8FGb4wG1VqL-qe0Q3mdn2MXZEIK5Xzgg3hI6fUtcD8uNzLGCOJ5YRFjigb84J1lJ6BUezJHXE",
+        "client-id": import.meta.env.VITE_PAYPAL_CLIENT_ID,
       }}
     >
       <PayPalButtons
