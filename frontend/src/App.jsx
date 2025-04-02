ba008bed059dcf9912ffbc24ac36d54cc5c8ad17
dushan <kddalwis@gmail.com>
2025-04-02 19:22:09 +0530

    complete the frontend of admin user management page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import OrderDetailsPage from "./pages/OrderDetailsPage";
 import MyOrdersPage from "./pages/MyOrdersPage";
 import AdminLayout from "./components/Admin/AdminLayout";
 import AdminHomePage from "./pages/AdminHomePage";
+import UserManagement from "./components/Admin/UserManagement";
 
 const App = () => {
   return (
@@ -50,6 +51,7 @@ const App = () => {
 
           <Route path="/admin" element={<AdminLayout />}>
             <Route index element={<AdminHomePage />}></Route>
+            <Route path="users" element={<UserManagement />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
