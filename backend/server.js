b828a5be7e20028d094cebe39d2793e15e1ecae8
dushan <kddalwis@gmail.com>
2025-04-03 16:08:02 +0530

    add the env file to the backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 9000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('WELCOME TO BUYZY API!');
